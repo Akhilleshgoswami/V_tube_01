ed27beb1f9fd472f274de81557abc2fe9078c3bc
Akhilesh <akhilesh@treflo.com>
2023-12-30 21:46:44 +0530

    setup file upload cloudinary and multer
    undefined

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -9,14 +9,14 @@ const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
     // upload the file on cloudinary
-    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+    const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    //  file has been uploaded successfully
-    console.log("File is uploded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation get failed
+    console.log(error);
+    fs.unlinkSync(localFilePath);
   }
 };
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
