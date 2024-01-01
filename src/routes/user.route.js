50b43deec1f835d5647fca19301328a64199cc81
Akhilesh <akhilesh@treflo.com>
2024-01-01 18:40:29 +0530

    add new routes for user controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { logInUser, registerUser,logOutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+  logInUser,
+  registerUser,
+  logOutUser,
+  refreshAccessToken,
+  changeCurrentPassowrd,
+  getCurrentUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getUserWatchHistory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -12,13 +23,23 @@ router.route("/register").post(
   ]),
   registerUser
 );
-router.route("/login").post(logInUser
-);
+router.route("/login").post(logInUser);
 
-//  secured routes 
-router.route("/logout").post(
-  verifyJWT,logOutUser
-);
-router.route("/refresh-token").post(refreshAccessToken
-);
+//  secured routes
+router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassowrd);
+// update route
+router.route("/upadte-account").patch(verifyJWT, getCurrentUser);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+// get route
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/histroy").get(verifyJWT, getUserWatchHistory);
+
 export default router;
